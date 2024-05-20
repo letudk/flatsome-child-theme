88ffa320227ec3ed803116f66cb43d6928f7b05f
Le Tu <121410236+letudk@users.noreply.github.com>
2024-05-20 13:57:04 +0700

    Update single.php

--- single.php
+++ single.php
@@ -20,7 +20,7 @@ foreach($post_categories as $c){
 ?>
 
 
-<div class="large-12 col notpadding" style="background-color: transparent;">
+<div class="large-12 col notpadding" style="background-color: transparent; padding: 0 !important;">
 	<div class="breadcrum">	</div>
    <main>
         <?php
