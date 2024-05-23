d354c51a8d30c8914c5168090c60928177aa5caf
Le Tu <121410236+letudk@users.noreply.github.com>
2024-05-23 20:23:24 +0700

    s

--- functions.php
+++ functions.php
@@ -736,6 +736,9 @@ function enqueue_child_theme_scripts() {
     // Enqueue Slick script
     // wp_enqueue_script('slick-js', 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js', false, null);
     // wp_enqueue_script('slick-js');
+    // Nhập các script khác nếu cần
+    wp_enqueue_script('theme-script', get_stylesheet_directory_uri() . '/assets/js/custom-script.js', array('jquery'), null, true);
+
 }
 add_action('wp_enqueue_scripts', 'enqueue_child_theme_scripts');
 
